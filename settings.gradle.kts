c2b4feca62cba841db2ad7f6adf5c136a78e01ca
wiem <ghars.wiem4@gmail.com>
2023-11-13 22:20:09 +0100

    finished project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Wiem-Ghars-LSI3-MesureGlycemie"
+include(":app")
+ 
