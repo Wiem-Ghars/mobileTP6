c833be6c9fd6a816f4ce152c061d28eae8d8f955
wiem <ghars.wiem4@gmail.com>
2023-12-09 21:28:31 +0100

    finished project

--- app/src/main/java/com/example/wiem_ghars_lsi3_mesureglycemie/view/HomeActivity.java
+++ app/src/main/java/com/example/wiem_ghars_lsi3_mesureglycemie/view/HomeActivity.java
@@ -6,16 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.wiem_ghars_lsi3_mesureglycemie.R;
+import com.example.wiem_ghars_lsi3_mesureglycemie.controller.LoginController;
 
 public class HomeActivity extends AppCompatActivity {
     private Button btnConsultation;
+    //TP6
+    private EditText etUserName;
+    private LoginController loginController;
+    private EditText etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        btnConsultation=(Button) findViewById(R.id.btnConcultation);
+        init();
+        etUserName.setText(loginController.getUserName());
+        etPassword.setText(loginController.getPassword());
+        btnConsultation=(Button) findViewById(R.id.btnConsultation);
         btnConsultation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,5 +34,10 @@ public class HomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private void init()
+    {
+         loginController = com.example.wiem_ghars_lsi3_mesureglycemie.controller.LoginController.getInstance(HomeActivity.this);
+        btnConsultation =(Button)
     }
 }
--- app/src/main/java/com/example/wiem_ghars_lsi3_mesureglycemie/view/MainActivity.java
+++ app/src/main/java/com/example/wiem_ghars_lsi3_mesureglycemie/view/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.wiem_ghars_lsi3_mesureglycemie.view;
 
 import com.example.wiem_ghars_lsi3_mesureglycemie.controller.Controller;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText etVal;
     private Button btn;
     private SeekBar sbAge;
-    private TextView tvAge, tvRes;
+    private TextView tvAge;
     private RadioButton rbtnYes, rbtnNo;
+    private final int REQUEST_CODE=1;
     //private Controller controller=new Controller();
     private Controller controller = Controller.getInstance();
 
@@ -71,7 +74,13 @@ public class MainActivity extends AppCompatActivity {
                             valMesuree = Float.valueOf(etVal.getText().toString());
                             boolean isFasting = rbtnYes.isChecked();
                             //user action view--> controller
-                            controller.createPatient(age,valMesuree,isFasting);}
+                            controller.createPatient(age,valMesuree,isFasting);
+                            //controller --> view
+                            //tvRes.setText(controller.getResult())
+                            Intent intent=new Intent(MainActivity.this,ConsultActivity.class);
+                            intent.putExtra("Reponse",controller.getResult());
+                            startActivityForResult(intent,REQUEST_CODE);
+                        }
 
 
                     }
@@ -80,12 +89,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_CODE)
+            if (resultCode==RESULT_CANCELED)
+                Toast.makeText(MainActivity.this,"Error(resultat est null)",Toast.LENGTH_SHORT).show();
+
+    }
+
     private void init() {
         etVal = (EditText) findViewById(R.id.etVal);
         sbAge = (SeekBar) findViewById(R.id.sbAge);
         btn = (Button) findViewById(R.id.btnConculter);
         tvAge = (TextView) findViewById(R.id.textAge);
-        tvRes = (TextView) findViewById(R.id.resultat);
         rbtnYes = (RadioButton) findViewById(R.id.rbtOui);
         rbtnNo = (RadioButton) findViewById(R.id.rbtNon);
 
